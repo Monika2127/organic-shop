326701e2307019f1a8af783ad004e15bfc02eedc
Monika2127 <47830785+Monika2127@users.noreply.github.com>
2023-10-17 23:15:03 +0530

    added sorting, fixed 1 err, datatable(x), search (#1)
    * added sorting, fixed 1 err, datatable(x), search
    
    * modify budget constraint & GA
    
    * github action

--- src/app/admin/ad-products/ad-products.component.ts
+++ src/app/admin/ad-products/ad-products.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { map } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, Subscription, map } from 'rxjs';
+import { Product } from 'src/app/models/products';
 import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
@@ -7,20 +8,65 @@ import { ProductService } from 'src/app/services/product/product.service';
   templateUrl: './ad-products.component.html',
   styleUrls: ['./ad-products.component.scss']
 })
-export class AdProductsComponent {
+export class AdProductsComponent implements OnDestroy {
 
   products$: any;
+  products: Product[] = [];
+  filteredProducts: any[] = [];
+  prodSubscription!: Subscription;
+  sortAsc: boolean = true;
+
+  dtOptions: DataTables.Settings = {}
+  dtTrigger: Subject<any> = new Subject<any>()
 
   constructor(
     private prodSer: ProductService,
   ) {
+    this.loadProducts();
+  }
 
+  loadProducts() {
     this.products$ = this.prodSer.getProducts().snapshotChanges().pipe(
       map(change => {
         return change.map((c) => ({ key: c.payload.key, ...(c.payload.val() as object) }))
       })
     );
-    
+
+    this.prodSubscription = this.products$.subscribe((item: any) => {
+      this.filteredProducts = this.products = item;
+    })
+
+  }
+
+  search(query: string) {
+    //  for searching the query in the client side, we have to save the data in an array and then render it and search it in that
+    //  array
+
+    this.filteredProducts = query ?
+      this.products.filter(item => item.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products
+  }
+
+
+  sortTable(event: Event): void {
+    const target = event.target as HTMLElement;
+    this.sortAsc = !this.sortAsc;
+
+    if (target.tagName === 'TH') {
+      const clickedHeader = target.innerText.toLowerCase(); // This will give you the text content of the clicked <th> element
+
+      if (!clickedHeader) return;
+
+      if (this.sortAsc)
+        this.filteredProducts.sort((a, b) => a[clickedHeader] >= b[clickedHeader] ? 1 : -1)
+      else
+        this.filteredProducts.sort((a, b) => a[clickedHeader] < b[clickedHeader] ? 1 : -1)
+    }
+  }
+
+
+  ngOnDestroy(): void {
+    this.prodSubscription.unsubscribe();
   }
 
 
